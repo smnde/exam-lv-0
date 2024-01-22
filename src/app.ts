82a08b01d9227a28b2455a9a344cf88c90909932
usman <des.mn69@gmail.com>
2024-01-22 22:49:48 +0700

    first commit

---
+++ src/app.ts
@@ -0,0 +1,114 @@
+// 1.
+export function minimal(a: number, b: number): number {
+	// console.log(a < b ? a : b);
+	// return a < b ? a : b;
+
+	if (a < b) return a;
+	else if (a > b) return b;
+	else return a;
+}
+
+// 2.
+export function power(a: number, b: number): number {
+	return Math.pow(a, b);
+}
+
+// 3.
+export function penjumlahanBilangan(a: number[], b: string): number | string {
+	if (b === "ganjil") {
+		const hasil: number = a
+			.filter((num: number) => num % 2 !== 0)
+			.reduce((acc, curr) => acc + curr, 0);
+
+		return hasil;
+	} else if (b === "genap") {
+		const hasil: number = a
+			.filter((num: number) => num % 2 === 0)
+			.reduce((acc, curr) => acc + curr, 0);
+
+		return hasil;
+	} else {
+		return "error";
+	}
+}
+
+// 4.
+class Animal {
+	name: string;
+	age: number;
+	isMamal: boolean;
+
+	constructor(name: string, age: number, isMamal: boolean) {
+		this.name = name;
+		this.age = age;
+		this.isMamal = isMamal;
+	}
+}
+
+// 5.
+class Rabbit extends Animal {
+	constructor(name: string, age: number, isMamal: boolean = true) {
+		super(name, age, isMamal);
+	}
+
+	eat(): string {
+		return `${this.name} yang berumur ${this.age} sedang makan!`;
+	}
+}
+
+// 6.
+class Eagle extends Animal {
+	speed: number;
+
+	constructor(
+		name: string,
+		age: number,
+		isMamal: boolean,
+		speed: number = 220
+	) {
+		super(name, age, isMamal);
+		this.speed = speed;
+	}
+
+	fly(): string {
+		return `${this.name} yang berumur ${this.age} sedang terbang!`;
+	}
+
+	protected run(speed: number) {
+		return `${this.name} berlari dengan kecepatan ${speed} km`;
+	}
+}
+
+// 7.
+class EagleRun extends Eagle {
+	constructor(
+		name: string,
+		age: number,
+		isMamal: boolean = false,
+		speed: number = 220
+	) {
+		super(name, age, isMamal, speed);
+	}
+
+	fly(): string {
+		return `${this.name} yang berumur ${this.age} sedang terbang!`;
+	}
+
+	runEagle(speed: number): string {
+		return `${this.name} berlari dengan kecepatan ${speed} km`;
+	}
+}
+
+// 8.
+const myRabbit: Rabbit = new Rabbit("Labi", 2);
+myRabbit.eat();
+
+// 9.
+const myEagle: Eagle = new Eagle("Elo", 4, false, 100);
+myEagle.fly();
+// myEagle.run(100); // pasti error
+
+// 10.
+const speedTes: EagleRun = new EagleRun("Labi", 4, false, 220);
+
+export { Animal, Rabbit, Eagle, EagleRun, myRabbit, myEagle, speedTes };
---
+++ tests/app.test.ts
@@ -0,0 +1,107 @@
+import {
+	Animal,
+	Eagle,
+	EagleRun,
+	Rabbit,
+	minimal,
+	myEagle,
+	myRabbit,
+	penjumlahanBilangan,
+	power,
+	speedTes,
+} from "../src/app";
+
+describe("minimal function", () => {
+	it("should return minimum number", () => {
+		expect(minimal(1, 4)).toBe(1);
+		expect(minimal(3, 2)).toBe(2);
+		expect(minimal(3, 3)).toBe(3);
+	});
+});
+
+describe("power func", () => {
+	it("should return a^b", () => {
+		expect(power(7, 3)).toBe(343);
+		expect(power(3, 3)).toBe(27);
+		expect(power(4, 0.5)).toBe(2);
+	});
+});
+
+describe("penjumlahan bilangan func", () => {
+	it("should return sum of array", () => {
+		expect(penjumlahanBilangan([1, 2, 3], "ganjil")).toBe(4);
+		expect(penjumlahanBilangan([1, 2, 3, 4, 5, 6, 7], "genap")).toBe(12);
+		expect(penjumlahanBilangan([1, 2, 3, 4, 5], "ganjil")).toBe(9);
+		expect(penjumlahanBilangan([1, 2, 3, 4, 5, 6, 7], "ganj")).toBe("error");
+		expect(penjumlahanBilangan([1, 2, 3, 4, 5, 6, 7], "positif")).toBe(
+			"errors"
+		);
+	});
+});
+
+describe("Class Animal", () => {
+	it("should create an instance of Animal", () => {
+		const dog = new Animal("dog", 5, false);
+
+		expect(dog).toBeInstanceOf(Animal);
+		expect(dog.name).toBe("dog");
+		expect(dog.age).toBe(5);
+		expect(dog.isMamal).toBe(false);
+	});
+
+	it("should create instance of rabbit", () => {
+		const rabbit = new Rabbit("rabbit", 5);
+
+		expect(rabbit).toBeInstanceOf(Rabbit);
+		expect(rabbit.name).toBe("rabbit");
+		expect(rabbit.age).toBe(5);
+		expect(rabbit.isMamal).toBe(true);
+		expect(rabbit.eat()).toBe("rabbit yang berumur 5 sedang makan!");
+	});
+
+	it("should create instance of Eagle", () => {
+		const eagle = new Eagle("eagle", 5, false);
+
+		expect(eagle).toBeInstanceOf(Eagle);
+		expect(eagle.name).toBe("eagle");
+		expect(eagle.age).toBe(5);
+		expect(eagle.isMamal).toBe(false);
+		expect(eagle.fly()).toBe("eagle yang berumur 5 sedang terbang!");
+		// expect(eagle.run(100)).toBe("eagle berlari dengan kecepatan 100 km"); // should be public
+	});
+
+	it("should create instance of EagleRun", () => {
+		const eagleRun = new EagleRun("eagle", 5, false);
+
+		expect(eagleRun).toBeInstanceOf(Eagle);
+		expect(eagleRun.name).toBe("eagle");
+		expect(eagleRun.age).toBe(5);
+		expect(eagleRun.isMamal).toBe(false);
+		expect(eagleRun.fly()).toBe("eagle yang berumur 5 sedang terbang!");
+		expect(eagleRun.runEagle(100)).toBe(
+			"eagle berlari dengan kecepatan 100 km"
+		);
+	});
+
+	it("should return instance of Rabbit", () => {
+		expect(myRabbit.name).toBe("Labi");
+		expect(myRabbit.age).toBe(2);
+		expect(myRabbit.isMamal).toBe(true);
+		expect(myRabbit.eat()).toBe("Labi yang berumur 2 sedang makan!");
+	});
+
+	it("should return instance of Eagle", () => {
+		expect(myEagle.name).toBe("Elo");
+		expect(myEagle.age).toBe(4);
+		expect(myEagle.isMamal).toBe(false);
+		expect(myEagle.fly()).toBe("Elo yang berumur 4 sedang terbang!");
+		// expect(myEagle.run(100)).not.toBe("Elo berlari dengan kecepatan 100 km"); // error
+	});
+
+	it("should return instance of EagleRun", () => {
+		expect(speedTes.name).toBe("Labi");
+		expect(speedTes.age).toBe(4);
+		expect(speedTes.isMamal).toBe(false);
+		expect(speedTes.runEagle(220)).toBe("Labi berlari dengan kecepatan 220 km");
+	});
+});
